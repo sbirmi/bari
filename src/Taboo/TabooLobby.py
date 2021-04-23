3782b19cb1f797d2eb3bfe44c63d8aa596c3fd2b
Sharad Birmiwal <sb-bari@sharadb.net>
2021-04-23 08:04:33 -0700

    TabooLobby: Parsing host-parameters
    Added the ability to interpret host-parameters and do basic validation.
    Note that no new game rooms get created at the moment.

---
+++ src/Taboo/TabooLobby.py
@@ -0,0 +1,48 @@
+"""Taboo lobby. This allows hosting/creating TabooRooms"""
+
+from fwk.Exceptions import InvalidDataException
+from fwk.GamePlugin import GameLobbyPlugin
+from fwk.Msg import (
+        ClientTxMsg,
+        InternalRegisterGi,
+)
+from fwk.MsgType import MTYPE_HOST_BAD
+
+from Taboo.HostParameters import HostParameters
+
+class TabooLobby(GameLobbyPlugin):
+    """Lobby for Taboo"""
+    gameIdx = 0
+    rooms = {}
+
+    def processHost(self, qmsg):
+        """
+        original_message = [
+            "HOST", "taboo",
+            {"numTeams": <int>,         # 1..4
+             "turnDurationSec": <int>,  # 30..180
+             "wordSets": ["name1", "name2", ...],
+             "allowLateJoinees": <bool>}]
+
+        qmsg.jmsg = original_message[2:]
+        """
+        try:
+            hostParameters = HostParameters.fromJmsg(qmsg.jmsg)
+        except InvalidDataException as exc:
+            self.txQueue.put_nowait(ClientTxMsg(
+                [MTYPE_HOST_BAD] + exc.toJmsg(), {qmsg.initiatorWs},
+                initiatorWs=qmsg.initiatorWs))
+            return True
+
+        self.gameIdx += 1
+
+        # TODO
+        #newRoom =
+        #self.rooms[self.gameIdx] = newRoom
+        #self.txQueue.put_nowait(InternalRegisterGi(newRoom, initiatorWs=qmsg.initiatorWs))
+
+        return True
+
+def plugin():
+    """Register with bari server"""
+    return TabooLobby("taboo", "Taboo")
