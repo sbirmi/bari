42eccaf6acab886d07c55afbbea3668154631cf0
lambdaFooBar <connect.jaggu@gmail.com>
2021-04-25 04:44:50 -0400

    [Taboo] Adding Player, Team, Room for Join request. WIP

---
+++ src/Taboo/TabooPlayer.py
@@ -0,0 +1,22 @@
+""" Taboo Player"""
+from fwk.MsgSrc import Connections
+from Taboo.TabooTeam import TabooTeam
+
+class TabooPlayer:
+
+    def __init__(self, txQueue, name, team):
+        """ Creates a taboo player associated to a team. Self-registers to the team
+
+        txQueue:
+        name: player name
+        team: team that this player should belong to
+        """
+        self.name = name
+        self.playerConns = Connections(txQueue)
+        team.addPlayer(self)
+
+    def addConn(self, ws):
+        self.playerConns.addConn(ws)
+
+    def delConn(self, ws):
+        self.playerConns.delConn(ws)
