6515c1600ed16b7e97af200268b9e98484793c40
Sharad Birmiwal <sb-bari@sharadb.net>
2021-04-28 06:36:31 -0700

    Taboo: Introduced notion of rounds
    - numRounds in HostParameters
    - roundsPlayed in player object (not sure how/if this will be useful)

--- src/Taboo/TabooPlayer.py
+++ src/Taboo/TabooPlayer.py
@@ -4,7 +4,7 @@ from Taboo.TabooTeam import TabooTeam
 
 class TabooPlayer:
 
-    def __init__(self, txQueue, name, team):
+    def __init__(self, txQueue, name, team, roundsPlayed=0):
         """ Creates a taboo player associated to a team. Self-registers to the team
 
         txQueue: tx queue
@@ -15,6 +15,7 @@ class TabooPlayer:
         self.playerConns = Connections(txQueue)
         team.addPlayer(self)
         self.team = team
+        self.roundsPlayed = roundsPlayed
 
     def addConn(self, ws):
         self.playerConns.addConn(ws)
